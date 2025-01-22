e242b0cd5782f0f80f9c50bacbfebbb71a69ad90
Jbarb25 <barboza205@usf.edu>
2025-01-22 16:24:48 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,9 @@
+let products= ["spoon", "fork", "knife", "plate", "bowl"];
+products.unshift("pan")
+products.pop();
+console.log(products)
+
+let scores= [47, 89, 91, 76, 82];
+scores[1]= 93;
+let scores_avg= (scores[0]+scores[1]+scores[2]+scores[3]+scores[4]) / scores.length;
+console.log(scores_avg)
