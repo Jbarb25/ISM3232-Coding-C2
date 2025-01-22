4ba28242c3326552655a50b811bfa5de2cb695ef
Jbarb25 <barboza205@usf.edu>
2025-01-22 16:34:55 -0500

    Task 3 - Employee Records

--- cc_2.js
+++ cc_2.js
@@ -1,5 +1,5 @@
 let products= ["spoon", "fork", "knife", "plate", "bowl"];
-products.unshift("pan")
+products.unshift("pan");
 products.pop();
 console.log(products)
 
@@ -7,3 +7,13 @@ let scores= [47, 89, 91, 76, 82];
 scores[1]= 93;
 let scores_avg= (scores[0]+scores[1]+scores[2]+scores[3]+scores[4]) / scores.length;
 console.log(scores_avg)
+
+let employee= {
+    name: "Mickey Mouse",
+    age: 56,
+    department: "entertainment",
+    isActive: true,}
+
+ employee.department= "sales";
+ employee.position= "manager";
+ console.log(employee)
